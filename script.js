e73cca661c9fbe2a8d037300a9c816048cfae9ac
Mina Selim <mina.selim2005@hotmail.com>
2018-01-30 15:28:06 -0500

    fixed small bug

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 var data;
-function updateValues()
+function updateValues(dat)
 {
   if (data[0].message.length > 0) {
     document.getElementById('red-1').style.visibility = "visible";
-    document.getElementById('red-1').innerHTML = data[0].message.length;
+    document.getElementById('red-1').innerHTML = dat[0].message.length;
+  }
+  else
+  {
+    document.getElementById('red-1').style.visibility = "hidden";
+    document.getElementById('red-1').innerHTML = dat[0].message.length;
   }
   if (data[1].message.length > 0) {
       document.getElementById('red-2').style.visibility = "visible";
-      document.getElementById('red-2').innerHTML = data[1].message.length;
+      document.getElementById('red-2').innerHTML = dat[1].message.length;
     }
+  else
+  {
+    document.getElementById('red-2').style.visibility = "hidden";
+    document.getElementById('red-2').innerHTML = dat[0].message.length;
+  }
   
 }
 
 
-document.getElementById("message_1").onclick = function()
+ function fun1()
 {
-  alert(data[0]);
-  $.get("elders/" + data[0] + "/delete")
+  alert(data[0].message[0]);
+  $.get("delete");
 }
+
 setInterval(function()
 {
   $.get("elders" , function(dat)
   {
     data = dat;
-    updateValues();
+    updateValues(dat);
   });
 }
 , 1000)
--- server.js
+++ server.js
@@ -17,6 +17,15 @@ var allElders = [new elder(1, "Mike") , new elder (2, "Mina" ), new elder(3, "To
 
 server.use('/img', express.static('img'));
 
+server.get('/delete' , function(req, res)
+{
+    console.log("calling delete");
+    var temp =  allElders[0];
+    temp.message.splice(0,  temp.message.length);
+        
+    
+    
+});
 server.get('/elders' , function(req, res)
 {
     res.send(allElders);
@@ -24,7 +33,7 @@ server.get('/elders' , function(req, res)
 
 server.get('/elders/:room', function(req, res)
 {
-    console.log(req.params.room)
+    
     res.send(allElders[req.params.room - 1])
 });
 server.get('/help/:id' , function(req,res)
@@ -37,21 +46,10 @@ server.get('/help/:id' , function(req,res)
 
 server.get('/elders/:room/:message' , function(req, res)
 {
-    let temp = allElders[req.params.room -1];
+    var temp = allElders[req.params.room -1];
     temp.message.push(req.params.message);
 })
-server.get('/elders/:room/:message/delete' , function(req, res)
-{
-    let temp =  allElders[req.params.room -1];
-    for(let i = 0; i < temp.message.length ; i++)
-    {
-            if(temp.message == req.params.message)
-        {
-            temp.message.splice(i,1);
-        }
-    }
-    
-});
+
 server.get('*', function(req, res){
     if(fs.existsSync(__dirname + req.url))
     {
